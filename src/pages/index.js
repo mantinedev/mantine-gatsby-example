5261896ba2fabca8c42490c31f928d4ee440f971
Vitaly Rtishchev <rtivital@gmail.com>
2021-05-01 11:34:52 +0300

    Init example

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { Button } from "@mantine/core"
+
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+    <Button>Hello</Button>
+  </Layout>
+)
+
+export default IndexPage
